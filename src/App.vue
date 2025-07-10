171573c5f906344ced9767024c6fc34beba2327d
fullstackboss <romeltejada@gmail.com>
2025-07-10 15:31:30 -0500

    1. Configuración Basica

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// Página de inicio limpia, sin componentes adicionales
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+/* Puedes agregar estilos globales o personalizados aquí si lo necesitas */
+</style>
